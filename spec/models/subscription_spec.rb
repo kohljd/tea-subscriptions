171049c3549842583ceb7d521fdb29b9ecf1b586
Jess Kohl <149989113+kohljd@users.noreply.github.com>
2024-05-05 17:31:33 -0500

    create Subscription model

--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -6,10 +6,10 @@ RSpec.describe Subscription, type: :model do
     it { should validate_presence_of :frequency }
   
     it { should validate_presence_of :price }
-    it { should validate_numericality_of(:price).is is_greater_than(0) }
+    it { should validate_numericality_of(:price).is_greater_than(0) }
     
     it { should validate_presence_of :status }
-    it { should define_enum_for(:status).with_values(:active, :cancelled) }
+    it { should define_enum_for(:status).with_values(active: 0, cancelled: 1) }
   end
 
   describe "relationships" do
