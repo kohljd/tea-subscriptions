48e0791177e12bb80093c896c375306049bffb33
Jess Kohl <149989113+kohljd@users.noreply.github.com>
2024-05-05 17:39:46 -0500

    test: TeaSubscription model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_05_221056) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_05_223648) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -34,6 +34,15 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_05_221056) do
     t.index ["customer_id"], name: "index_subscriptions_on_customer_id"
   end
 
+  create_table "tea_subscriptions", force: :cascade do |t|
+    t.bigint "tea_id", null: false
+    t.bigint "subscription_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["subscription_id"], name: "index_tea_subscriptions_on_subscription_id"
+    t.index ["tea_id"], name: "index_tea_subscriptions_on_tea_id"
+  end
+
   create_table "teas", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -44,4 +53,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_05_221056) do
   end
 
   add_foreign_key "subscriptions", "customers"
+  add_foreign_key "tea_subscriptions", "subscriptions"
+  add_foreign_key "tea_subscriptions", "teas"
 end
